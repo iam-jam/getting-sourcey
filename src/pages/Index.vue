1e093cd13da3c2922ca227fdc40554c56ee16c84
James Hall <james-hall@gmx.co.uk>
2020-01-27 17:22:53 +0000

    List the sauces on the homepage.

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,7 +1,45 @@
 <template>
   <Layout>
-
-    <h1>Getting Saucey!</h1>
-
+    <article v-for="sauce in $page.sauces.edges" :key="sauce.node.id" class="sauce-list-item">
+      <img class="sauce-list-item__image" :src="sauce.node.image" />
+      <div>
+        <h2><g-link :to="sauce.node.path">{{ sauce.node.name }}</g-link></h2>
+        <p>£{{ sauce.node.price }}</p>
+      </div>
+    </article>
   </Layout>
 </template>
+
+<page-query>
+query {
+  sauces: allSauce {
+    edges {
+      node {
+        id
+        path
+        name
+        image
+        price
+      }
+    }
+  }
+}
+</page-query>
+
+
+<style>
+  .sauce-list-item {
+    display: flex;
+    padding: 1rem 0;
+    border-bottom: 2px solid #eee;
+    margin-bottom: 1rem;
+  }
+  body {
+    font-size: 1rem;
+  }
+  .sauce-list-item__image {
+    display: block;
+    flex-grow: 0;
+    max-width: 100px;
+  }
+</style>
