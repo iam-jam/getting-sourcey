e88395d6339c3009625b798ee3ccd97bd670b63a
James Hall <james-hall@gmx.co.uk>
2020-01-27 16:53:04 +0000

    Load the saucey data.
    Use axios to fetch the saucey data from our API and then use the datastore API to insert into our collections.

--- gridsome.server.js
+++ gridsome.server.js
@@ -5,8 +5,13 @@
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
+const axios = require('axios')
+
 module.exports = function (api) {
-  api.loadSource(({ addCollection }) => {
+  api.loadSource(async ({ addCollection }) => {
+
+    const apiUrl = process.env.API_URL
+
     // Set up our collections
     const sauce = addCollection('Sauce')
     const user = addCollection('User')
@@ -15,5 +20,42 @@ module.exports = function (api) {
     // And our references
     review.addReference('sauce', 'Sauce')
     review.addReference('user', 'User')
+
+    // Finally load the data...
+
+    // Sauces
+    const { data:sauces } = await axios.get(`${apiUrl}/sauces.json`)
+    for (const item of sauces) {
+      sauce.addNode({
+        id: item.id,
+        name: item.name,
+        image: item.image,
+        price: item.price,
+        tags: item.tags
+      })
+    }
+
+    // Users
+    const { data:users } = await axios.get(`${apiUrl}/users.json`)
+    for (const item of users) {
+      user.addNode({
+        id: item.id,
+        name: item.name,
+        image: item.image
+      })
+    }
+
+    const { data:reviews } = await axios.get(`${apiUrl}/reviews.json`)
+    for (const item of reviews) {
+      review.addNode({
+        id: item.id,
+        sauce: item.sauce,
+        user: item.user,
+        stars: item.stars,
+        review: item.review,
+        date: item.date
+      })
+    }
+
   })
 }
