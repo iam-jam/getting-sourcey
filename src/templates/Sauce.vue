b0a6816713463450bae3db28eaea84e0ccd426b1
James Hall <james-hall@gmx.co.uk>
2020-01-27 17:21:28 +0000

    Create a template for the sauce collection nodes.

---
+++ src/templates/Sauce.vue
@@ -0,0 +1,80 @@
+<template>
+  <Layout>
+    <section>
+      <article class="sauce">
+        <h1>{{ $page.sauce.name }}</h1>
+        <img class="sauce__image" :src="$page.sauce.image" />
+        <p>£{{ $page.sauce.price }}</p>
+      </article>
+    </section>
+
+    <section>
+      <h2>Reviews</h2>
+      <article
+        class="review-list-item"
+        v-for="review in $page.sauce.belongsTo.edges"
+        :key="review.node.id"
+      >
+        <img class="review-list-item__image" :src="review.node.user.image" />
+        <div>
+          <p>{{ review.node.date }}</p>
+          <p>{{ review.node.stars }}</p>
+          <h3>{{ review.node.review }}</h3>
+        </div>
+      </article>
+    </section>
+  </Layout>
+</template>
+
+<page-query>
+query ($id: ID!) {
+  sauce(id: $id) {
+    name
+    image
+    price
+    belongsTo(
+      filter: { typeName: { eq: Review } }
+      sortBy: "date"
+      order: DESC
+    ) {
+      edges {
+        node {
+          ... on Review {
+            user {
+              name
+              image
+            }
+            date(format: "YYYY-MM-DD")
+            stars
+            review
+          }
+        }
+      }
+    }
+  }
+}
+</page-query>
+
+<style>
+.sauce {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.sauce__image {
+  max-width: 300px;
+  height: auto;
+  margin-bottom: 2rem;
+}
+.review-list-item {
+  display: flex;
+  padding: 1rem 0;
+  margin-bottom: 1rem;
+}
+.review-list-item__image {
+  max-width: 100px;
+  height: auto;
+  flex-grow: 0;
+  margin-right: 1rem;
+}
+</style>
