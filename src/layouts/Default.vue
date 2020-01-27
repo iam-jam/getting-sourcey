27a413b8504959793e2a79b113f993fb270299d1
James Hall <james-hall@gmx.co.uk>
2020-01-27 17:23:37 +0000

    Create the site layout and set some global styles.

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,5 +1,32 @@
 <template>
-  <div>
-    <slot/>
+  <div class="container">
+    <header><g-link to="/">Saucey reviews</g-link></header>
+    <main><slot /></main>
   </div>
 </template>
+
+<style>
+  .container {
+    max-width: 650px;
+    margin: 0 auto;
+  }
+  body {
+    font-family: Rubik;
+  }
+  header {
+    font-family: Pacifico;
+    text-align: center;
+    font-size: 2.5rem;
+    margin-bottom: 3rem;
+  }
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+
+  h1, h2 {
+    font-size: 2rem;
+    margin-top: 0;
+    margin-bottom: 0.5rem;
+  }
+</style>
